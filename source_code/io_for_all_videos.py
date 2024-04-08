1c0b0c7749e190c4063a2b8bbc7ddacecc099b6f
Marie <marie.bn007@gmail.com>
2024-04-08 17:28:14 +0200

    fix: this script now handles .MP4 files such as recorded by professionnal cameras

--- source_code/io_for_all_videos.py
+++ source_code/io_for_all_videos.py
@@ -14,6 +14,8 @@ assert os.path.exists(data_path), "Wrong PATH"
 
 directories = os.listdir(data_path)
 
+time_in_seconds = False
+
 for directory in directories:
 
     try:
@@ -23,11 +25,13 @@ for directory in directories:
 
     for filename in filenames:
 
-        if ".mp4" in filename:
+        if (".mp4" in filename or ".MP4" in filename) and filename[0]!="_": # is a video and is not a working file
 
             video_name = filename[:-4]
+            extension = filename[-4:]
             print(f'Currently processing video "{video_name}".')
-            video_io = VideoIO(f'{data_path}/{directory}/', video_name, verbose=10)
+            video_io = VideoIO(f'{data_path}/{directory}/', video_name, extension=extension, verbose=10)
+            video_io.save_config()
 
             if (
                 video_io.spatial_crop['x1']==0 and 
@@ -36,13 +40,16 @@ for directory in directories:
                 video_io.spatial_crop['y2']==video_io.video.frame_height
             ):
                 video_io.get_spatial_crop_input_from_user()
+
+            video_io.process(force=True)
             if video_io.time_crop[0]==0 and video_io.time_crop[1]==video_io.oflow_len:
-                video_io.auto_time_crop()
+                video_io.auto_time_crop()      
 
             Color.reset()
-            video_io.process(force=True)
-            video_io.draw_diagrams()
+            video_io.draw_diagrams(time_in_seconds=time_in_seconds)
             video_io.to_animation()
 
+            vid = video_io.record_video()
+
 tf = time()
-print(f'Computation took {int(tf-t0)} seconds.') # less than two minutes for 10 videos
+print(f'Computation took {int(tf-t0)} seconds.') # less than two minutes for 10 videos # way longer if longer videos or higher fps -> up to 10 minutes per video
