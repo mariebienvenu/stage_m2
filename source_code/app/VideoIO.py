8d26cbabff6440dcdc916fef3dc530602e56c9cf
Marie <marie.bn007@gmail.com>
2024-04-08 17:26:07 +0200

    feat: one more diagram, both frame and second time units handled, and more

--- source_code/app/VideoIO.py
+++ source_code/app/VideoIO.py
@@ -4,6 +4,7 @@ import os, json
 import numpy as np
 from tqdm import tqdm
 from plotly.subplots import make_subplots
+import cv2
 
 import app.Video as Video
 import app.Animation as Animation
@@ -24,20 +25,20 @@ def default_config(height, width, frame_count, fps):
         },
         "time crop":{
             "start":0,
-            "stop":frame_count-1,
+            "stop":int(frame_count-1),
         },
         "image processing method":"gray",
-        "background proportion":0.97,
-        "frame rate":fps,
+        "background proportion":0.0,
+        "frame rate":fps, #unused
     }
 
 
 class VideoIO:
 
 
-    def __init__(self, directory, video_name, verbose=0):
+    def __init__(self, directory, video_name, extension='.mp4', verbose=0):
 
-        self.video = Video.Video(f'{directory}/{video_name}'+'.mp4', verbose=verbose)
+        self.video = Video.Video(f'{directory}/{video_name}'+extension, verbose=verbose)
         self.name = video_name
         self.directory = directory
 
@@ -86,7 +87,7 @@ class VideoIO:
     
     def complete_config(self):
         default = self.get_default_config()
-        for key, value in enumerate(default):
+        for key, value in default.items():
             if key not in self.config:
                 self.config[key] = value
 
@@ -108,13 +109,17 @@ class VideoIO:
         return self.config['background proportion']
     
     @property
+    def frame_rate(self) -> float:
+        return self.config['frame rate']
+    
+    @property
     def oflow_len(self):
         return self.video.frame_count - 1
     
 
     def get_frame_times(self): # TODO move to Video ?
-        return np.array(list(range(self.oflow_len)), dtype=np.float64)/self.video.fps
-
+        return np.array(list(range(self.oflow_len)), dtype=np.float64)/self.frame_rate
+    
     
     def process(self, force=True):
         if self.is_processed and not force:
@@ -141,7 +146,7 @@ class VideoIO:
         self.is_processed = True
 
 
-    def draw_diagrams(self, fig=None, save=True, show=False):
+    def draw_diagrams(self, fig=None, save=True, show=False, time_in_seconds=False): # can be drawn either in frame scale or seconds scale
         fig = make_subplots(rows=2, cols=3, subplot_titles=(
             "Amplitude du flux au cours du temps",
             "Vitesses au cours du temps" ,
@@ -154,7 +159,7 @@ class VideoIO:
         if not self.is_processed:
             self.process()
 
-        frame_times = self.get_frame_times()
+        frame_times = self.get_frame_times() if time_in_seconds else np.array(list(range(self.oflow_len)))
 
         vis.magnitude_angle(frame_times, self.magnitude_means, self.magnitude_stds, self.angle_means, self.angle_stds, fig=fig, rows=[1,2], cols=[1,1])
         vis.add_curve(y=self.velocity_y, x=self.position_y, name="y'=f(y) - Portrait de phase de Y", fig=fig, col=3, row=1)
@@ -166,37 +171,51 @@ class VideoIO:
         vis.add_curve(y=self.position_y, x=self.position_x, name="y=f(x) - Trajectoire", fig=fig, col=3, row=2)
 
         rows, cols = (1,1,2,2), (1,2,1,2)
+        start = self.time_crop[0]/self.frame_rate if time_in_seconds else self.time_crop[0]
+        stop = self.time_crop[1]/self.frame_rate if time_in_seconds else self.time_crop[1]
         for row, col in zip(rows, cols):
-            fig.add_vline(x=self.time_crop[0], row=row, col=col)
-            fig.add_vline(x=self.time_crop[1], row=row, col=col)
+            fig.add_vline(x=start, row=row, col=col)
+            fig.add_vline(x=stop, row=row, col=col)
 
         fig.update_layout(title=f'Optical flow  - {self.name}')
         fig.write_html(self.directory+f"/{self.name}_diagram.html") if save else None
         fig.show() if show else None
 
-        return fig
+        fig2 = vis.magnitude_angle(frame_times, self.magnitude_means, self.magnitude_stds, self.angle_means, self.angle_stds)
+        rows2, cols2 = (1,2), (1,1)
+        for row, col in zip(rows2, cols2):
+            fig2.add_vline(x=start, row=row, col=col)
+            fig2.add_vline(x=stop, row=row, col=col)
+        fig2.write_html(self.directory+f"/{self.name}_oflow.html") if save else None
+        fig2.show() if show else None
+
+        fig3 = vis.add_curve(y=self.position_y, x=self.position_x, name="y=f(x) - Trajectoire")
+        fig3.write_html(self.directory+f"/{self.name}_trajectory.html") if save else None
+        fig3.show() if show else None
+
+        return [fig, fig2, fig3]
 
 
-    def to_animation(self, save=True):
+    def to_animation(self, save=True): # always in frame scale
         if not self.is_processed:
             self.process()
-        frame_times = self.get_frame_times()
+        times = np.array(list(range(self.oflow_len)))
         anim = Animation.Animation([
-            Curve.Curve(np.vstack((frame_times, self.magnitude_means)).T, fullname='Oflow magnitude - mean'),
-            Curve.Curve(np.vstack((frame_times, self.magnitude_stds)).T, fullname='Oflow magnitude - std'),
-            Curve.Curve(np.vstack((frame_times, self.angle_means)).T, fullname='Oflow angle - mean'),
-            Curve.Curve(np.vstack((frame_times, self.angle_stds)).T, fullname='Oflow angle - std'),
-            Curve.Curve(np.vstack((frame_times, self.velocity_x)).T, fullname='Velocity X'),
-            Curve.Curve(np.vstack((frame_times, self.velocity_y)).T, fullname='Velocity Y'),
-            Curve.Curve(np.vstack((frame_times, self.position_x)).T, fullname='Location X'),
-            Curve.Curve(np.vstack((frame_times, self.position_y)).T, fullname='Location Y'),
+            Curve.Curve(np.vstack((times, self.magnitude_means)).T, fullname='Oflow magnitude - mean'),
+            Curve.Curve(np.vstack((times, self.magnitude_stds)).T, fullname='Oflow magnitude - std'),
+            Curve.Curve(np.vstack((times, self.angle_means)).T, fullname='Oflow angle - mean'),
+            Curve.Curve(np.vstack((times, self.angle_stds)).T, fullname='Oflow angle - std'),
+            Curve.Curve(np.vstack((times, self.velocity_x)).T, fullname='Velocity X'),
+            Curve.Curve(np.vstack((times, self.velocity_y)).T, fullname='Velocity Y'),
+            Curve.Curve(np.vstack((times, self.position_x)).T, fullname='Location X'),
+            Curve.Curve(np.vstack((times, self.position_y)).T, fullname='Location Y'),
         ])
         anim.crop(start=self.time_crop[0], stop=self.time_crop[1])
         anim.save(self.directory + self.name + '/') if save else None
         return anim
 
 
-    def get_spatial_crop_input_from_user(self, save=True):
+    def get_spatial_crop_input_from_user(self, save=True): # TODO maybe should be here instead of in video ?
         crop = self.video.get_spatial_crop_input_from_user()
         self.config['spatial crop'] = crop
         self.save_config() if save else None
@@ -206,7 +225,21 @@ class VideoIO:
     def auto_time_crop(self, patience=2, save=True):
         if not self.is_processed:
             self.process()
-        start, stop = oflow.get_crop(self.get_frame_times(), self.magnitude_means)
-        self.config['time crop'] = {'start':start, 'stop':stop}
+        times = np.array(list(range(self.oflow_len)))
+        start, stop = oflow.get_crop(times, self.magnitude_means)
+        self.config['time crop'] = {'start':int(start), 'stop':int(stop)} # int32 -> int because int32 not json serialisable
         self.save_config() if save else None
         return start, stop
+    
+    def record_video(self):
+        frame_count = self.time_crop[1]-self.time_crop[0]+1
+        frame_height = self.spatial_crop["y2"]-self.spatial_crop["y1"]
+        frame_width = self.spatial_crop["x2"]-self.spatial_crop["x1"]
+        content = np.zeros((frame_count, frame_height, frame_width))
+        for i,index in enumerate(range(self.time_crop[0], self.time_crop[1]+1)):
+            frame = self.video.get_frame(index, image_processing=self.image_processing_method, crop=self.spatial_crop)
+            content[i,:,:] = np.copy(frame)
+        video = Video.Video.from_array(content, self.directory+f'/_{self.name}_preprocessed.mp4', fps=self.video.fps)
+        return video
+
+        
