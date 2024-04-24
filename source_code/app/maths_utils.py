fix: computation of derivative by finite differences now properly tested

source_code/app/maths_utils.py
@@ -4,24 +4,19 @@ import scipy.stats as stats
 from scipy.integrate import RK45 as runge_kutta
 import scipy.integrate as integrate
 
-def derivee(array, step):
+def derivee(array, step:int|float):
     ar = np.array(array)
     res = (ar[2:]-ar[:-2])/(2*step) # différences finies
-    #left = res[:-1]
-    #right = res[1:]
-    #for i in range(left.size):
-    #    if left[i]!=0 and right[i]!=0 and int(left[i]/abs(left[i])) != int(right[i]/abs(right[i])):
-    #        res[i]=0 # sign change means an actual derivative of 0
     return res
 
 
-def derivee_seconde(array, step): # TODO m_utils.derivee_seconde() -- à tester dans maths.py
+def derivee_seconde(array, step:int|float):
     ar = np.array(array)
     res = (-2*ar[1:-1] + ar[:-2] + ar[2:])/step**2  # différences finies
     return res
 
 
-def integrale1(array, step):
+def integrale1(array, step:int|float):
     arr = np.array(array)
     res = np.zeros_like(arr)
     curr = 0
@@ -30,7 +25,7 @@ def integrale1(array, step):
         res[i+1] = curr
     return res
 
-def integrale3(array, step):
+def integrale3(array, step:int|float):
     x = np.array(list(range(np.array(array).size)))/step
     res = [0]
     for i in range(1, x.size):

source_code/testing/maths.py
@@ -4,7 +4,27 @@ from random import random
 import pandas as pd
 import plotly.graph_objects as go
 
+import app.visualisation as vis
+
 DO_SHOW = True
+
+## Test of derivatives
+
+from app.maths_utils import derivee, derivee_seconde
+
+step = 0.02
+x = np.arange(0,5,step)
+y = np.cos(x)
+
+dy = derivee(y, step)
+ddy = derivee_seconde(y, step)
+
+fig = go.Figure()
+vis.add_curve(y=y, x=x, fig=fig, name="y = cos(x)")
+vis.add_curve(y=dy, x=x, fig=fig, name="y'(x) -> -sin(x)")
+vis.add_curve(y=ddy, x=x, fig=fig, name="y''(x) -> -cos(x)")
+
+if DO_SHOW: fig.show()
 
 ## Test of correlation
 
@@ -28,7 +48,6 @@ print(correlation(arr1, arr5)) # expect 1
 ## Test of Dynamic Time Warping
 
 from app.maths_utils import dynamic_time_warping
-import app.visualisation as vis
 
 # SIMPLE CASE
 y1 = np.array([1, 2, 3, 2, 3, 2, 1, 1])