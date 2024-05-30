a0e4710243c5252c99ca2b56e54f625dbac8a1f7
Marie <marie.bn007@gmail.com>
2024-05-30 15:04:41 +0200

    quick test to save figures as static images

---
+++ source_code/testing/plotly.py
@@ -0,0 +1,11 @@
+
+import app.visualisation as vis
+import plotly
+import kaleido
+
+print(plotly.__version__, kaleido.__version__)
+
+fig = vis.add_curve(y=[1,3,4,2,0])
+
+fig.write_image("C:/Users/Marie Bienvenu/stage_m2/afac/1.png")  #, height=1080, width=1920, scale=2, validate=True)
+fig.show()
