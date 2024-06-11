9e6891f93e68fe3fde4e65cbf7e12755d2a5cdcf
Marie <marie.bn007@gmail.com>
2024-06-11 11:06:48 +0200

    clean

--- source_code/types/scene_types.py
+++ source_code/types/scene_types.py
@@ -1,4 +1,5 @@
-from typing import Protocol, List
+
+from typing import Protocol
 from enum import Enum
 
 from types.curve_types import AnimationCurve
@@ -16,8 +17,8 @@ class RotationOrder(Enum):
 class Scene(Protocol):
     """All objects' rotations are assumed to be modeled by Euler angles."""
 
-    def get_all_animated_objects(self) -> List[str] : """Lists all the controllers in the scene which hold animation curves."""
-    def get_animated_attributes(self, object_name:str) -> List[str] : """Lists all the animated attributes of an animation controller. An attribute is considered animated if it has at least two keys."""
+    def get_all_animated_objects(self) -> list[str] : """Lists all the controllers in the scene which hold animation curves."""
+    def get_animated_attributes(self, object_name:str) -> list[str] : """Lists all the animated attributes of an animation controller. An attribute is considered animated if it has at least two keys."""
     def get_rotation_order(self, object_name:str) -> RotationOrder : ...
     def get_animation_curve(self, object_name:str, attribute:str) -> AnimationCurve : ...
     def get_time_range(self) -> tuple[int, int] : """The scene's time range as defined by the time slider."""
