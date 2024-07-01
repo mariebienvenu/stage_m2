6b1c63f5f578a811d977785d8b5cfa8e07bb44c3
Marie <marie.bn007@gmail.com>
2024-07-01 14:46:35 +0200

    feat: updated test of internalProces to test the make_diagrams method

--- source_code/testing/testing_internal_process.py
+++ source_code/testing/testing_internal_process.py
@@ -56,85 +56,10 @@ for curve, name in zip([vcurve1, vcurve2, bcurve1, banim2[0]], ["Feature Ref", "
 
 if DO_SHOW: fig0.show()
 
-# The matches between the feature curves with different degrees of selection
-
-def visualise_matches(internal:InternalProcess):
-    Color.reset()
-
-    dtw = internal.dtw
-    pairs = dtw.pairings
-    constraints = internal.dtw_constraints
-
-    naive_indexes = [0] + [index for index in range(1, len(pairs)-1) if constraints[index]>InternalProcess.COST_THRESHOLD] + [len(pairs)-1]
-    refined_indexes = dtw.filtered_indexes()
-
-    fig1 = make_subplots(
-        rows=1, cols=3, 
-        shared_xaxes='all', 
-        subplot_titles=["No filter", "Basic filter (1 criteria)", "Refined filter  (several criteria)"],
-        vertical_spacing=0.1, horizontal_spacing=0.1,
-    )
-
-    color1, color2 = Color.next(), Color.next()
-
-    x1, y1, x2, y2 = dtw.times1, dtw.values1, dtw.times2, dtw.values2+4
-    all_pairings = pairs
-    naive_pairings = [e for i,e in enumerate(pairs) if i in naive_indexes]
-    refined_pairings = [e for i,e in enumerate(pairs) if i in refined_indexes]
-    for col in [1,2, 3]:
-        vis.add_pairings(y2=y2, x2=x2, y1=y1, x1=x1, pairs=all_pairings, color=(210, 210, 210), opacity=1, fig=fig1, row=1, col=col)
-        vis.add_curve(y=y2, x=x2, name="curve1", color=color2, fig=fig1, row=1, col=col)
-        vis.add_curve(y=y1, x=x1, name="curve2", color=color1, fig=fig1, row=1, col=col)
-    vis.add_pairings(y2=y2, x2=x2, y1=y1, x1=x1, pairs=all_pairings, color="green", opacity=1, fig=fig1, row=1, col=1)
-    vis.add_pairings(y2=y2, x2=x2, y1=y1, x1=x1, pairs=naive_pairings, color="green", opacity=1, fig=fig1, row=1, col=2)
-    vis.add_pairings(y2=y2, x2=x2, y1=y1, x1=x1, pairs=refined_pairings, color="green", opacity=1, fig=fig1, row=1, col=3)
-
-    fig1.update_layout(
-        xaxis1_title="Time (frames)",
-        xaxis2_title="Time (frames)",
-        xaxis3_title="Time (frames)",
-        yaxis_title="Amplitude (arbitrary)",
-    )
-    title="ALL VS Basic VS Refined matches"
-    fig1.update_layout(title=title)
-    if DO_SHOW: fig1.show()
-    return fig1
-
-visualise_matches(internal)
-
-## Visualise the inliers and outliers on the shortest path with DTW cost as a background
-
-def visualise_graph(internal:InternalProcess):
-    Color.reset()
-    dtw = internal.dtw
-    pairs = dtw.pairings
-    inliers = internal.kept_indexes
-    outliers = getattr(internal, "outliers", [])
-    fig2 = vis.add_heatmap(pd.DataFrame(dtw.cost_matrix))
-    inlier_color, outlier_color = Color.next(), Color.next()
-    for index  in inliers:
-        i,j = pairs[index][1]-pairs[0][1], pairs[index][0]-pairs[0][0]
-        fig2.add_shape(
-            type="circle",
-            x0=i-2, y0=j-2, x1=i+2, y1=j+2,
-            line_color=Color.to_string(inlier_color),
-            name=f'{index}',
-        )
-    for index  in outliers:
-        i,j = pairs[index][1]-pairs[0][1], pairs[index][0]-pairs[0][0]
-        fig2.add_shape(
-            type="circle",
-            x0=i-2, y0=j-2, x1=i+2, y1=j+2,
-            line_color=Color.to_string(outlier_color),
-            name=f'{index}',
-        )
-    vis.add_curve(y=np.array(pairs)[:,0]-pairs[0][0], x=np.array(pairs)[:,1]-pairs[0][1], fig=fig2)
-    title = "Shortest path across cost matrix"
-    fig2.update_layout(title=title)
-    if DO_SHOW: fig2.show()
-    return fig2
-
-visualise_graph(internal)
+figures, titles = internal.make_diagrams(number_issues=False)
+for fig, title in zip(figures, titles):
+    fig.update_layout(title=title)
+    if DO_SHOW: fig.show()
 
 ## Now, what about the case where there is a number issue ?
 print("CASE WHERE THERE IS ONE TOO MANY IMPULSE IN THE REFERENCE CURVE")
@@ -158,10 +83,12 @@ for curve, name in zip([vcurve1, new_curve1, new_vcurve2, bcurve1, new_banim2[0]
 
 if DO_SHOW: fig2.show()
 
-visualise_matches(new_internal)
+figures, titles = new_internal.make_diagrams(number_issues=False)
+for fig, title in zip(figures, titles):
+    fig.update_layout(title=title)
+    if DO_SHOW: fig.show()
 
 new_internal.detect_number_issues()
-visualise_graph(new_internal)
 
 ## Let's try to fix this
 
@@ -176,8 +103,10 @@ for curve, name in zip([fixed_internal.curve1, fixed_curve1, fixed_internal.curv
     curve.display(fig=fig3, handles=False, style="lines+markers", name=name)
 if DO_SHOW: fig3.show()
 
-visualise_matches(fixed_internal)
-fig4 = visualise_graph(fixed_internal)
+figures, titles = fixed_internal.make_diagrams(number_issues=True)
+for fig, title in zip(figures, titles):
+    fig.update_layout(title=title)
+    if DO_SHOW: fig.show()
 
 constraints = fixed_internal.dtw_constraints
 curves = [fixed_internal.curve1, fixed_curve1, fixed_internal.curve2, bcurve1, fixed_banim2[0]]
@@ -216,10 +145,12 @@ for curve, name in zip([vcurve1, new_curve1, new_vcurve2, bcurve1, new_banim2[0]
 
 if DO_SHOW: fig2.show()
 
-visualise_matches(new_internal)
+figures, titles = new_internal.make_diagrams(number_issues=False)
+for fig, title in zip(figures, titles):
+    fig.update_layout(title=title)
+    if DO_SHOW: fig.show()
 
 new_internal.detect_number_issues()
-visualise_graph(new_internal)
 
 ## Let's try to fix this
 
@@ -234,5 +165,7 @@ for curve, name in zip([fixed_internal.curve1, fixed_curve1, fixed_internal.curv
     curve.display(fig=fig3, handles=False, style="lines+markers", name=name)
 if DO_SHOW: fig3.show()
 
-visualise_matches(fixed_internal)
-fig5 = visualise_graph(fixed_internal)
+figures, titles = fixed_internal.make_diagrams(number_issues=True)
+for fig, title in zip(figures, titles):
+    fig.update_layout(title=title)
+    if DO_SHOW: fig.show()
