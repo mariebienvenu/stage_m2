4e8e1912715e9dfac335297b3174b5be193aa521
Marie <marie.bn007@gmail.com>
2024-07-01 17:16:05 +0200

    feat: more diagrams ; also cleanup

--- source_code/app/internal_process.py
+++ source_code/app/internal_process.py
@@ -1,8 +1,8 @@
 
-from typing import List # TODO should be removed, useless ; and legacy class too
 from copy import deepcopy
 import numpy as np
 from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 from app.animation import Animation
 import app.warping as W
@@ -15,9 +15,9 @@ import app.visualisation as vis
 
 
 class InternalProcess:
-    # TODO : do some update of Main (and debug using test_main) to make this makeover work in the pipeline
 
-    COST_THRESHOLD = 2
+    COST_THRESHOLD = 7
+    CONSTRAINT_THRESHOLD = 2
     AREA_THRESHOLD = 0.1
     LOCAL_WINDOW_SIZE = 10
 
@@ -34,10 +34,10 @@ class InternalProcess:
         self.select(feature)
         if normalize: self.normalize_curves()
         self.do_dtw()
-        issue = False
+        self.issue_detected = False
         if detect_issue:
-            issue = self.detect_number_issues()
-            if issue:
+            self.issue_detected = self.detect_number_issues()
+            if self.issue_detected:
                 self.operator = self.process_number_issues(blend=blend_if_issue)
                 self.new_curve1 = self.operator(self.curve1)
                 if normalize: self.normalize_curves()
@@ -46,7 +46,7 @@ class InternalProcess:
                 #if still_issue: raise RecursionError(f"Internal Process failed to solve outlier issues. \n\t Outliers:{self.outliers} (expected [])")
                 if still_issue and self.verbose>0: print(f"Internal Process failed to solve outlier issues. \n\t Outliers:{self.outliers} (expected [])")
         self.make_warp(filter=filter_indexes, interpolation=warp_interpolation)
-        self.make_new_anim(channels, use_operator=issue)
+        self.make_new_anim(channels, use_operator=self.issue_detected)
         return self.banim2
 
 
@@ -75,7 +75,7 @@ class InternalProcess:
     def detect_number_issues(self):
         # TODO find a better name for the function, beurk
         self.former_outliers = getattr(self, "outliers", None)
-        self.outliers = self.dtw.detect_limitation(InternalProcess.COST_THRESHOLD, InternalProcess.COST_THRESHOLD, InternalProcess.AREA_THRESHOLD, InternalProcess.LOCAL_WINDOW_SIZE)
+        self.outliers = self.dtw.detect_limitation(InternalProcess.COST_THRESHOLD, InternalProcess.CONSTRAINT_THRESHOLD, InternalProcess.AREA_THRESHOLD, InternalProcess.LOCAL_WINDOW_SIZE)
         if self.verbose>0: print(f"Internal Process : found {len(self.outliers)} outliers in shortest path.")
         return len(self.outliers)!=0
 
@@ -184,13 +184,23 @@ class InternalProcess:
                 yaxis_title="Amplitude (arbitrary)",
             )
             return fig
-
+        
+        def aux_along_path(dtw:DynamicTimeWarping, constraints:np.ndarray, inliers:list[int], outliers:list[int], rectangle_width=1):
+            fig = go.Figure()
+            rect_indexes = inliers+outliers
+            rect_colors = [inlier_color]*len(inliers) + [outlier_color]*len(outliers)
+            for index, color in zip(rect_indexes, rect_colors):
+                fig.add_vrect(x0=index-rectangle_width/2, x1=index+rectangle_width/2, opacity=0.8, line_width=0, fillcolor=Color.to_string(color))
+            vis.add_curve(y=constraints, name="contribution as saved cost", fig=fig)
+            vis.add_curve(y=dtw.cost_along_shortest_path, name="instant cost", fig=fig)
+            return fig
+            
         dtw = self.dtw
         constraints = self.dtw_constraints
         pairs = dtw.pairings
         inliers = self.kept_indexes
         outliers = getattr(self, "outliers", [])
-        naive_indexes = [0] + [index for index in range(1, len(pairs)-1) if constraints[index]>InternalProcess.COST_THRESHOLD] + [len(pairs)-1]
+        naive_indexes = [0] + [index for index in range(1, len(pairs)-1) if constraints[index]>InternalProcess.CONSTRAINT_THRESHOLD] + [len(pairs)-1]
         
         fig1 = aux_enriched_map(dtw, pairs, inliers, outliers)
         title1 = "Cost matrix with shortest path and selected nodes"
@@ -198,8 +208,11 @@ class InternalProcess:
         fig2 = aux_matches_filter(dtw, pairs, naive_indexes, inliers)
         title2 = "Matches with no or simple or refined selection"
 
-        figures = [fig1, fig2]
-        titles = [title1, title2]
+        fig3 = aux_along_path(dtw, constraints, inliers, outliers, 2)
+        title3 = "Cost and saved cost along shortest path"
+
+        figures = [fig1, fig2, fig3]
+        titles = [title1, title2, title3]
 
         if not number_issues : return figures, titles
 
@@ -208,76 +221,30 @@ class InternalProcess:
         former_pairs = former_dtw.pairings
         former_outliers = self.former_outliers
         former_inliers = former_dtw.filtered_indexes()
-        former_naive_indexes = [0] + [index for index in range(1, len(former_pairs)-1) if former_constraints[index]>InternalProcess.COST_THRESHOLD] + [len(former_pairs)-1]
+        former_naive_indexes = [0] + [index for index in range(1, len(former_pairs)-1) if former_constraints[index]>InternalProcess.CONSTRAINT_THRESHOLD] + [len(former_pairs)-1]
 
-        fig3 = aux_enriched_map(former_dtw, former_pairs, former_inliers, former_outliers)
-        title3 = "Cost matrix with shortest path and selected nodes - before"
+        fig4 = aux_enriched_map(former_dtw, former_pairs, former_inliers, former_outliers)
+        title4 = "Cost matrix with shortest path and selected nodes - before"
 
-        fig4 = aux_matches_filter(former_dtw, former_pairs, former_naive_indexes, former_inliers)
-        title4 = "Matches with no or simple or refined selection - before"
+        fig5 = aux_matches_filter(former_dtw, former_pairs, former_naive_indexes, former_inliers)
+        title5 = "Matches with no or simple or refined selection - before"
 
-        figures = [fig3, fig4, fig1, fig2]
-        titles = [title3, title4, title1+" - after", title2+" - after"]
+        fig6 = make_subplots(rows=2, cols=2, shared_xaxes=True, subplot_titles=["Before", "After"])
+        self.curve1.display(handles=False, style="lines+markers", color=ref_color, fig=fig6, row=1, col=1)
+        self.new_curve1.display(handles=False, style="lines+markers", color=ref_color, fig=fig6, row=2, col=1)
+        title6 = "Reference feature curve - before and after processing"
+        
+        fig7, title7 = go.Figure(), ""
+        if len(self.banim1)>0:
+            fig7 = make_subplots(rows=2, cols=2, shared_xaxes=True, subplot_titles=["Before", "After"])
+            self.banim1.display(fig=fig7, row=1, col=1)
+            self.new_banim1.display(fig=fig7, row=2, col=1)
+            title7 = "Original animation curve - before and after processing"
+
+        fig8 = aux_along_path(former_dtw, former_constraints, former_inliers, former_outliers, 2)
+        title8 = "Cost and saved cost along shortest path - before"
+
+        figures = [fig6, fig7, fig8, fig4, fig5, fig1, fig2, fig3]
+        titles = [title6, title7, title8, title4, title5, title1+" - after", title2+" - after", title3+" - after"]
 
         return figures, titles
-
-
-
-class LegacyInternalProcess:
-
-    def __init__(self, vanim_ref:Animation, vanim_target:Animation, banim:Animation):
-        self.vanim1 = vanim_ref
-        self.vanim2 = vanim_target
-        self.banim1 = banim
-    
-
-    def make_warp(self, feature:str=None, only_temporal=True, interpolation="linear", verbose=0):
-        self.feature = feature
-        curve1 = self.vanim1.find(feature)
-        curve2 = self.vanim2.find(feature)
-        if not only_temporal: raise NotImplementedError
-        curve1.normalize()
-        curve2.normalize()
-        self.dtw = DynamicTimeWarping(curve1, curve2) # performs the DTW algo at init time
-        time_in, time_out = self.dtw.bijection
-        warp = W.make_warp(dimension=1, interpolation=interpolation, X_in=time_in, X_out=time_out)
-        return warp
-    
-
-    def make_simplified_warp(self, feature:str=None, only_temporal=True, constraint_threshold=1.5, local_scale=10, interpolation="linear", verbose=0):
-        self.feature = feature
-        curve1 = self.vanim1.find(feature)
-        curve2 = self.vanim2.find(feature)
-        if not only_temporal: raise NotImplementedError
-        curve1.normalize()
-        curve2.normalize()
-        self.dtw = DynamicTimeWarping(curve1, curve2) # performs the DTW algo at init time
-        time_in, time_out = self.dtw.bijection
-        self.dtw_constraints = self.dtw.local_constraints(local_scale) if local_scale>0 else self.dtw.global_constraints() #measure_dtw_constraint(local=True, window_size=local_scale)
-        self.kept_indexes = [0]
-        zipped = zip(self.dtw_constraints[:-2], self.dtw_constraints[1:-1], self.dtw_constraints[2:])
-        for i,(left,constraint,right) in enumerate(zipped):
-            if constraint > max(constraint_threshold, left, right):
-                self.kept_indexes.append(i+1)
-        self.kept_indexes.append(self.dtw_constraints.size-1)
-        if len(self.kept_indexes)==2:
-            if verbose>0: print(f"Warp simplification did not work ; no index has a certainty better than {constraint_threshold}. Reverting to classic warp computation.")
-            return self.make_warp(feature, only_temporal, interpolation, verbose=verbose)
-        warp = W.make_warp(dimension=1, interpolation=interpolation, X_in=time_in[self.kept_indexes], X_out=time_out[self.kept_indexes])
-        return warp
-        
-    
-    def make_new_anim(self, channels:List[str]=None, warps:List[W.AbstractWarp]=None):
-        banim2 = Animation()
-        for curve in self.banim1:
-            if curve.fullname in channels:
-                warp = warps[channels.index(curve.fullname)]
-                banim2.append(curve.apply_spatio_temporal_warp(warp, in_place=False))
-            else:
-                banim2.append(curve)
-        return banim2
-
-
-    def measure_dtw_constraint(self, local=True, window_size=10): # TODO - remove, useless
-        if local: return self.dtw.local_constraints(window_size)
-        else : return self.dtw.global_constraints()
